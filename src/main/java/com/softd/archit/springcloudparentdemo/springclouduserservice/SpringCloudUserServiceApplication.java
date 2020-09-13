a40c15e1ba8037ae2c7b53240f9d51505422cf4f
cobee <18027041863@163.com>
2020-09-13 23:07:02 +0800

    提交用户源码

--- src/main/java/com/softd/archit/springcloudparentdemo/springclouduserservice/SpringCloudUserServiceApplication.java
+++ src/main/java/com/softd/archit/springcloudparentdemo/springclouduserservice/SpringCloudUserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -15,7 +16,8 @@ import org.springframework.web.client.RestTemplate;
 //})
 //@EnableEurekaClient
 @EnableDiscoveryClient
-@EnableCircuitBreaker
+//@EnableCircuitBreaker
+@EnableHystrix
 public class SpringCloudUserServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/softd/archit/springcloudparentdemo/springclouduserservice/service/UserService.java
+++ src/main/java/com/softd/archit/springcloudparentdemo/springclouduserservice/service/UserService.java
@@ -1,8 +1,12 @@
 package com.softd.archit.springcloudparentdemo.springclouduserservice.service;
 
+import com.netflix.hystrix.HystrixCommandProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.hystrix.contrib.javanica.conf.HystrixPropertiesManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.HystrixProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,7 +23,9 @@ public class UserService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getUserOrderInfoFallback")
+    @HystrixCommand(fallbackMethod = "getUserOrderInfoFallback", commandProperties = {
+            @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_STRATEGY, value = "THREAD")
+    })
     public String getUserOrderInfo(String orderId){
         return restTemplate.getForObject("http://orderservice/order/getOrder/{orderId}", String.class, orderId);
     }
